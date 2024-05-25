e415a006004b6645a710b7ca635e1b3f2322b7df
Ahmed Ramdan <ahmedramdan5062@gmail.com>
2024-05-25 20:21:48 +0300

    solve bug

--- lib/Features/Login_Using_Phone/logic/all_logic.dart
+++ lib/Features/Login_Using_Phone/logic/all_logic.dart
@@ -4,8 +4,7 @@ import '../interface/screens/enter_the_otp.dart';
 
 class SCREENLOGIC {
   static submentPhone(BuildContext context) {
-    Navigator.push(
-      context,
+    Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) {
           return const INSERTOTP();
