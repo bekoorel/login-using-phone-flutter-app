680316ac2b255eeb8549e81a5f1f3266c61f7467
Ahmed Ramdan <ahmedramdan5062@gmail.com>
2024-05-25 20:24:55 +0300

    commint

--- lib/Features/Login_Using_Phone/logic/all_logic.dart
+++ lib/Features/Login_Using_Phone/logic/all_logic.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../interface/screens/enter_the_otp.dart';
 
 class SCREENLOGIC {
+  //bug
   static submentPhone(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
