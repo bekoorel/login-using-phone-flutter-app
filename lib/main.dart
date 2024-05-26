45e50a52acf30158b64d6d7b34c9cf89f896fa9b
Ahmed Ramdan <ahmedramdan5062@gmail.com>
2024-05-27 02:42:45 +0300

    added firebase and fix firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:login_using_phone/router/page_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+ // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
