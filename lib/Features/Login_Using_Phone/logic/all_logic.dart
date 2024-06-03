30c234f997bd4968c4ecadc96bc2be25633a3b70
Ahmed Ramdan <ahmedramdan5062@gmail.com>
2024-06-03 16:00:13 +0300

    added river pod test

--- lib/Features/Login_Using_Phone/logic/all_logic.dart
+++ lib/Features/Login_Using_Phone/logic/all_logic.dart
@@ -1,3 +1,4 @@
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -8,54 +9,47 @@ import 'package:login_using_phone_v2/Features/Login_Using_Phone/logic/firebase_p
 class LOGICLOGIN {
   PHONEAUTH phoneauthclass = PHONEAUTH();
 
-  submentPhone(BuildContext context, String phone) async{
-   
- // await  phoneauthclass.sendCode(phone,context);
-      context.go('/INSERTOTP');
-    
+  submentPhone(BuildContext context, String phone) async {
+    // await  phoneauthclass.sendCode(phone,context);
+    context.go('/INSERTOTP');
   }
 
   submentotp(BuildContext context, String smsCode) {
-    
-      // phoneauthclass.verifyCode(smsCode);
- context.go('/mainscreen');
-    
-  
+    // phoneauthclass.verifyCode(smsCode);
+    context.go('/mainscreen');
   }
 
   logout(BuildContext context) {
-   
-      // phoneauthclass.logout();
-      context.go('/');
-  
+    // phoneauthclass.logout();
+    context.go('/');
   }
 }
 
-final countdownProvider = StateNotifierProvider<COUNTEROTP,dynamic>((ref) {
+final countdownProvider = NotifierProvider<COUNTEROTP, int>(() {
   return COUNTEROTP();
 });
 
-
-class COUNTEROTP extends StateNotifier{
-   COUNTEROTP(): super(0);
-
-  int tcounter = 60;
-  Timer? _timer;
-
-  void initState() {
-    _startTimer();
+class COUNTEROTP extends Notifier<int> {
+  int tcounter = 0;
+  @override
+  int build() {
+    return tcounter;
   }
 
-   _startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (tcounter > 0) {
-        tcounter--;
+  void increment() {
+    tcounter++;
+    state = tcounter;  // تحديث الحالة لإعلام المستمعين بالتغيير
+  }
+   void startTimer() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (state == 60) {
+       
       } else {
-        _timer?.cancel();
-      //  tcounter==0;
+     state = tcounter++;
       }
-    
     });
   }
 
+
 }
+
